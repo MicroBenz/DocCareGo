d075e66aa942466e16aac78f1f0474c0fcc62155
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-10-11 14:14:30 +0700

    Nav: Complete mock up nav base on role

--- web/src/app/nav/nav.component.ts
+++ web/src/app/nav/nav.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
     selector: 'apps-nav',
     templateUrl: './nav.view.html',
     styles: [`
+        .top-nav > .container{
+            border-bottom: 1px solid #f3f3f3;
+        }
+        .menu-nav > .container a {
+            line-height: 34px;
+        }
         .profile-circle {
             width: 40px;
             height: 40px;
@@ -22,4 +28,30 @@ import { Component } from '@angular/core';
         }
     `]
 })
-export class NavComponent {}
+export class NavComponent {
+    public userRole: string;
+    public userRoleDisplay: string;
+    public userName: string;
+    private roleMapping = {
+        'patient': 'ผู้ป่วย',
+        'doctor': 'แพทย์',
+        'nurse': 'พยาบาล',
+        'staff': 'เจ้าหน้าที่',
+        'pharmacist': 'เภสัชกร'
+    }
+    constructor() {
+        if (window.localStorage['doccareGo'] === undefined || window.localStorage['doccareGo'] === null) {
+            this.userRole = 'doctor';
+            this.userRoleDisplay = this.roleMapping[this.userRole];
+        }
+        else if (window.localStorage['doccareGo']['role'] === undefined) {
+            this.userRole = 'staff'
+            this.userRoleDisplay = 'เจ้าหน้าที่';
+        }
+        else {
+            this.userRole = window.localStorage['doccareGo']['role'];
+            this.userRoleDisplay = this.roleMapping[window.localStorage['doccareGo']['role']]
+        }
+        this.userName = 'จอห์น';
+    }
+}
