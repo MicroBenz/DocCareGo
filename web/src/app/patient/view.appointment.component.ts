c738ebdf734c13222e81a66c445c3f51cd5599f4
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-10-12 21:14:15 +0700

    View Appointment [Patient]: Change variables name

--- web/src/app/patient/view.appointment.component.ts
+++ web/src/app/patient/view.appointment.component.ts
@@ -13,5 +13,10 @@ import { Component } from '@angular/core';
     `]
 })
 export class ViewAppointmentComponent {
-    public testAppointment = []
+    public appointments = [
+        {'date': '11 ม.ค. 2559', 'time': '13:00-15:00', 'clinic': 'ระบบประสาทและสมอง'},
+        {'date': '12 ม.ค. 2559', 'time': '13:00', 'clinic': 'ทางเดินอาหารและตับ'},
+        {'date': '13 ม.ค. 2559', 'time': '13:00', 'clinic': 'GGSE'},
+        {'date': '14 ม.ค. 2559', 'time': '13:00', 'clinic': 'ทดสอบแผนกยาวมากมากมากยาวโคตรๆ'}  
+    ]
 }
