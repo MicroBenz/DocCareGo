67ba4d9466d654b647e9dfd632e39087dfdc4973
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-10-24 14:05:38 +0700

    Nurse: Fix CSS

--- web/src/app/role/nurse/view.today.patient.component.ts
+++ web/src/app/role/nurse/view.today.patient.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { VIEW_TODAY_PATIENT_TITLE } from '../../config/title.config';
+
 @Component({
     selector: 'view-today-patient',
     templateUrl: './view.today.patient.view.html',
@@ -40,7 +43,7 @@ import { Component } from '@angular/core';
             height: 490px;
             overflow-y: scroll;
             margin-left: -20px;
-            margin-right: -20px;
+            margin-right: -35px;
             padding-left: 20px;
             padding-right: 20px;
         }
@@ -60,66 +63,72 @@ import { Component } from '@angular/core';
         }
     `]
 })
-export class ViewTodayPatientComponent {
-    public patientList = [
-        {
-            id: 1,
-            hn: '000022',
-            name: 'นายธนนันท์ ตั้งธนาชัยกุล',
-            doctor: 'นายแพทย์ธีรัช รักษ์เถา',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '10:00 - 10:30'
-        },
-        {
-            id: 2,            
-            hn: '103022',
-            name: 'นางสาววิภวานี วัชระเดชสกุล',
-            doctor: 'นายแพทย์ธีรัช รักษ์เถา',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '11:00 - 11:30'
-        },
-        {
-            id: 3,            
-            hn: '204052',
-            name: 'นายธีรัช รักษ์เถา',
-            doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '11:30 - 12:00'
-        },
-        {
-            id: 4,            
-            hn: '123456',
-            name: 'นายณัษฐพงษ์ อู่สิริมณีชัย',
-            doctor: 'นายแพทย์ธีรัช รักษ์เถา',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '12:00 - 12:30'
-        },
-        {
-            id: 5,            
-            hn: '654321',
-            name: 'นางสาววันทนีย์ ทองทั่ว',
-            doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '13:00 - 13:30'
-        },
-        {
-            id: 6,            
-            hn: '785912',
-            name: 'นายธนวัฒน์ เค้าฉลองเคียง',
-            doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
-            clinic: 'ทางเดินอาหารและตับ',
-            date: '13/1/2559',
-            time: '16:30 - 17:30'
-        }
-    ]
+export class ViewTodayPatientComponent implements OnInit {
+    public patientList;
+    private selectedPatient;
 
-    private selectedPatient = {};
+    constructor (private title: Title) {}
 
+    ngOnInit () {
+        this.patientList = [
+            {
+                id: 1,
+                hn: '000022',
+                name: 'นายธนนันท์ ตั้งธนาชัยกุล',
+                doctor: 'นายแพทย์ธีรัช รักษ์เถา',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '10:00 - 10:30'
+            },
+            {
+                id: 2,            
+                hn: '103022',
+                name: 'นางสาววิภวานี วัชระเดชสกุล',
+                doctor: 'นายแพทย์ธีรัช รักษ์เถา',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '11:00 - 11:30'
+            },
+            {
+                id: 3,            
+                hn: '204052',
+                name: 'นายธีรัช รักษ์เถา',
+                doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '11:30 - 12:00'
+            },
+            {
+                id: 4,            
+                hn: '123456',
+                name: 'นายณัษฐพงษ์ อู่สิริมณีชัย',
+                doctor: 'นายแพทย์ธีรัช รักษ์เถา',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '12:00 - 12:30'
+            },
+            {
+                id: 5,            
+                hn: '654321',
+                name: 'นางสาววันทนีย์ ทองทั่ว',
+                doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '13:00 - 13:30'
+            },
+            {
+                id: 6,            
+                hn: '785912',
+                name: 'นายธนวัฒน์ เค้าฉลองเคียง',
+                doctor: 'นายแพทย์ณัษฐพงษ์ อู่สิริมณีชัย',
+                clinic: 'ทางเดินอาหารและตับ',
+                date: '13/1/2559',
+                time: '16:30 - 17:30'
+            }
+        ]
+        this.selectedPatient = {};
+        this.title.setTitle(VIEW_TODAY_PATIENT_TITLE);
+    }
     onSelectPatient(idx) {
         this.selectedPatient = this.patientList[idx];
     }
