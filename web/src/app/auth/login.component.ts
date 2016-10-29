de82345bfbc13c92837ce1685fd246dadefa0a7e
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-10-29 19:57:48 +0700

    Login: Redesign login page

--- web/src/app/auth/login.component.ts
+++ web/src/app/auth/login.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { AuthService } from './../shared/service/auth.service';
@@ -13,73 +12,49 @@ import { LOGIN_TITLE } from './../config/title.config';
             display: table-cell;
             vertical-align: middle;
             text-align: center;
+
+            background-image: url('assets/img/login_bg3.jpg');
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
         }
         #login-view {
             width: 100%;
         }
         #login-view #web-logo {
-            width: 230px;
-            margin-bottom: 25px;
-        }
-        #login-box {
-            width: 350px;
-            margin: auto;
-        }
-        #login-box input {
-            width: 100%;
-            padding: 10px;
-            text-align: center;
-            border: 1px solid #00a250;
-            margin-bottom: 10px;
-            box-shadow: none;
-        }
-
-        #login-box button {
-            width: 100%;
-            border-radius: 3px;
+            width: 400px;
             margin-bottom: 15px;
         }
-        #login-box .register-text {
-            font-size: 19px;
-        }
-        #login-box .register-button {
-            color: #00a250;
-        }
-        #login-box .register-button:hover {
-            cursor: pointer;
-        }
-        .button {
-            border: none;
-            border-radius:  0px;
-            transition: all 0.2s ease-in-out;
-        }
-        .is-primary {
-            color: #ffffff;
-            background-color: #00a250;
-            height: 41px;
-        }
-        .is-primary:hover, .is-primary:active {
-            color: #ffffff;
-            background-color: #00a250;
-        }
-        .is-primary:hover {
+        #login-box {
+            width: 420px;
+            margin: auto;
+            padding: 25px;
+            background-color: #ffffff;
+            border-radius: 5px;
             box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        }
+        #login-box .control input, #login-box .button {
+            width: 100%;
         }
     `]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     public usr: string;
     public pwd: string;
-    public isLoginFail: boolean = false;
+    public isLoginFail: boolean;
 
-    constructor(private authService: AuthService, private router: Router, private titleService: Title) {
+    constructor(private authService: AuthService, private router: Router, private titleService: Title) {}
+
+    ngOnInit () {
         if (this.authService.hasLogin()) {
             this.router.navigateByUrl('/' + this.authService.getUserRole());
         }
-        titleService.setTitle(LOGIN_TITLE);
+        this.titleService.setTitle(LOGIN_TITLE);
+        this.isLoginFail = false;
     }
 
-    private login () {
+    public login () {
         //TODO: Made Login
         if (this.usr === 'patient' || this.usr === 'doctor' || this.usr === 'staff' || this.usr ==='nurse' || this.usr === 'pharmacist' || this.usr === 'admin') {
             this.isLoginFail = false;
