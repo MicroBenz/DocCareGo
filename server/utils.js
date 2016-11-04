98b5d52110557ecf8643fd08a485c24ca0126294
Nattapong Ousirimaneechai <first852456@gmail.com>
2016-11-04 13:44:23 +0700

    API: add utils.js for coding API

---
+++ server/utils.js
@@ -0,0 +1,20 @@
+module.exports = {
+    methodNotAllowed: methodNotAllowed,
+    responseMissingField: responseMissingField
+};
+
+function methodNotAllowed (req, res) {
+        res.status(405).send({
+            success: false,
+            status: 'Method Not Allowed',
+            message: 'This method is not allowed for this API.'
+        });
+    }
+
+function responseMissingField (res, field) {
+    res.status(400).send({
+        success: false,
+        status: 'Bad Request',
+        message: field + ' field is required.'
+    });
+}
