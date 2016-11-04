ae5582ed0906b23e252575c2f9fe11713b8f5dd3
Nattapong Ousirimaneechai <first852456@gmail.com>
2016-11-04 13:44:39 +0700

    Auth: add full auth

--- server/api/auth.api.js
+++ server/api/auth.api.js
@@ -1,24 +1,52 @@
 module.exports = function (app, express) {
     var jwt = require('jsonwebtoken');
+    var bcrypt = require('bcrypt-nodejs');
+    var User = require('../model/User');
     var authRoutes = express.Router();
     
     authRoutes.post('/login', login);
 
     function login (req, res) {
-        var user = {
-            'username': req.body.usr,
-            'role': req.body.role
-        };
-        var token = jwt.sign(user, app.get('tokenSecret'));
-        res.json({
-            success: true,
-            message: 'Login Success',
-            clientMessage: 'Login Success',
-            data: {
-                token: token,
-                role: req.body.role
+        User.findOne({
+            username: req.body.username
+        })
+        .then(
+            function (user) {
+                if (!user) {
+                    user = {
+                        password: bcrypt.hashSync('')
+                    };
+                }
+                
+                if (!bcrypt.compareSync(req.body.password, user.password)) {
+                    res.json({
+                        success: false,
+                        message: 'Login Failed!',
+                        clientMessage: 'Wrong Username or Password'
+                    });
+                }
+                else {
+                    user = user.toObject();
+                    delete user.password;
+                    var token = jwt.sign(user, app.get('tokenSecret'), {
+                        expiresIn: 1440*60
+                    });
+
+                    res.json({
+                        success: true,
+                        message: 'Login Success',
+                        data: {
+                            token: token,
+                            role: user.role
+                        }
+                    });
+                }
+            },
+            function (error) {
+                console.log(error);
+                throw error;
             }
-        });
+        );
     }
 
     app.use('/auth', authRoutes);
