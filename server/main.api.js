05539f9bdc2507f716ab1218164d302d56c1e5d8
Nattapong Ousirimaneechai <first852456@gmail.com>
2016-11-04 13:56:20 +0700

    API: add more default api

--- server/main.api.js
+++ server/main.api.js
@@ -5,5 +5,17 @@ module.exports = function (app, express) {
     // See example in test.api.js
     require('./api/auth.api')(app, express);
     require('./api/test.api')(apiRoutes, express);
+    require('./api/appointment.api')(apiRoutes, express);
+    require('./api/clinic.api')(apiRoutes, express);
+    require('./api/diagnosisResult.api')(apiRoutes, express);
+    require('./api/disease.api')(apiRoutes, express);
+    require('./api/medicine.api')(apiRoutes, express);
+    require('./api/nurse.api')(apiRoutes, express);
+    require('./api/patient.api')(apiRoutes, express);
+    require('./api/patientRecord.api')(apiRoutes, express);
+    require('./api/pharmacist.api')(apiRoutes, express);
+    require('./api/staff.api')(apiRoutes, express);
+    require('./api/user.api')(apiRoutes, express);
+    require('./api/workday.api')(apiRoutes, express);
     app.use('/api/v1', apiRoutes);
 };
