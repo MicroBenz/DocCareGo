ed8b0fd4c342ebfeb04a3b89241f6e4cecfe0835
Nattapong Ousirimaneechai <first852456@gmail.com>
2016-11-05 12:07:18 +0700

    Utils: add method checkRole(req, res, arrayOfRole)

--- server/utils.js
+++ server/utils.js
@@ -1,7 +1,24 @@
 module.exports = {
+    checkRole: checkRole,
     methodNotAllowed: methodNotAllowed,
     responseMissingField: responseMissingField
 };
+
+function checkRole (req, res, arrayOfRole) {
+    let isCorrectedRole = false;
+    arrayOfRole.forEach(
+        function (role) {
+            if(req.decoded.role === role)
+                isCorrectedRole = true;
+        }
+    );
+    if(!isCorectedRole) {
+        res.status(400).send({
+            status: 'Bad Request',
+            message: 'This API is not allowed for your role.'
+        });
+    }
+}
 
 function methodNotAllowed (req, res) {
     res.status(405).send({
