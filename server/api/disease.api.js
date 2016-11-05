0c664d26d8929beb8f78e6de8edf40934a7960ad
Wipavanee Vatcharadejsakul <ballwipavanee@gmail.com>
2016-11-05 15:23:44 +0700

    disease api

--- server/api/disease.api.js
+++ server/api/disease.api.js
@@ -1,3 +1,266 @@
-module.exports = (app, express) => {
+module.exports = function (apiRoutes, express) {
+    var diseaseRoutes = express.Router();    
+    var Disease = require('../model/Disease.js');
+    var utils = require('../utils.js');
 
+    diseaseRoutes.route('/')
+        .get(getDiseases)
+        .post(createDisease)
+        .put(utils.methodNotAllowed)
+        .delete(utils.methodNotAllowed);
+    
+    diseaseRoutes.route('/:name')
+        .get(getDiseaseByName)
+        .post(utils.methodNotAllowed)
+        .put(updateDiseaseByName)
+        .delete(deleteDiseaseByName);
+
+    apiRoutes.use('/diseases', diseaseRoutes);
+
+    // Implementation of CRUD are below.
+    //----------------- GET -----------------
+    function getDiseases (req, res) {
+        utils.checkRole(req, res, ['admin']);
+        var filterField = req.query.filters;
+        if (filterField) {
+            filterField = filterField.split(',').join(' ');
+        }
+        else {
+            filterField = '';
+        }
+        if (req.query.search) {
+            Disease.find({
+                $or: [
+                    {
+                        name: {
+                            $regex: req.query.search,
+                            $options: 'i'
+                        }
+                    }
+                ]
+            })
+            .select(filterField)
+            .then(
+                function (results) {
+                    res.json({
+                        success: true,
+                        data: results
+                    });
+                },
+                function (error) {
+                    console.log(error);
+                    res.status(500).send({
+                        success: false,
+                        message: error,
+                        clientMessage: 'Cannot get disease data.'
+                    });
+                }
+            );
+        }
+        else {
+            Disease.find().select(filterField).limit(10).then(
+                function (results) {
+                    res.json({
+                        success: true,
+                        limit: 10,
+                        data: results
+                    });
+                },
+                function (error) {
+                    console.log(error);
+                    res.status(500).send({
+                        success: false,
+                        message: error,
+                        clientMessage: 'Cannot get disease data.'
+                    });
+                }
+            );
+        }
+    }
+
+    function getDiseaseByName (req, res) {
+        utils.checkRole(req, res, ['admin','doctor']);
+        Disease.findOne({
+            name: req.params.name
+        })
+        .then(
+            function (disease) {
+                if (!disease) {
+                    res.status(400).send({
+                        success: false,
+                        message: 'Bad Request',
+                        clientMessage: 'No disease with this name.'
+                    });
+                }
+                else {
+                    res.json({
+                        success: true,
+                        data: disease
+                    });
+                }
+            },
+            function (error) {
+                console.log(error);
+                res.status(500).send({
+                    success: false,
+                    message: error,
+                    clientMessage: 'Cannot get disease data.'
+                });
+            }
+        );
+    }
+
+    //----------------- POST (CREATE) -----------------
+    function createDisease (req, res) {
+        utils.checkRole(req, res, ['admin']);
+        if (!req.body.name) {
+            utils.responseMissingField(res, 'name');
+        }
+
+        valNameateField(res, req.body);
+        Disease.findOneWithDeleted({
+            $or: [
+                { name: req.body.name }
+            ]
+        })
+        .then(
+            function (disease) {
+                if (disease) {
+                    res.status(400).send({
+                        success: false,
+                        message: 'Bad Request',
+                        clientMessage: 'This name has already in the system'
+                    });
+                    mongoose.Promise.reject(400);
+                }
+            },
+            function (error) {
+                console.log(error);
+                res.status(500).send({
+                    success: false,
+                    message: error,
+                    clientMessage: 'Cannot get disease data.'
+                });
+            }
+        )
+        .then(
+            function (disease) {
+                res.json({
+                    success: true,
+                    clientMessage: 'Create disease succeed',
+                    data: disease
+                });
+            },
+            function (error) {
+                res.status(500).send({
+                    success: false,
+                    clientMessage: 'Create disease failed',
+                    message: error
+                });
+            }
+        );
+    }
+
+    //----------------- PUT (UPDATE) -----------------
+    function updateDiseaseByName (req, res) {
+        utils.checkRole(req, res, ['admin']);
+        valNameateField(res, req.body);
+        var diseaseRef;
+        Disease.findOne({
+            name: req.params.name
+        })
+        .then(
+            function (data) {
+                diseaseRef.name = data.name;
+                diseaseRef.icd10 = data.icd10;
+                diseaseRef.snowmed = data.snowmed;
+                diseaseRef.drg = data.drg;
+                diseaseRef.description = data.description;
+                return diseaseRef.save();
+            }
+        )
+        .then(
+            function (disease) {
+                res.json({
+                    success: true,
+                    clientMessage: 'Update disease succeed',
+                    data: disease
+                });
+            },
+            function (error) {
+                res.status(500).send({
+                    success: false,
+                    clientMessage: 'Update disease failed',
+                    message: error
+                });
+            }
+        );
+    }
+
+    //----------------- DELETE -----------------    
+    function deleteDiseaseByName (req, res) {
+        utils.checkRole(req, res, ['admin']);
+        Disease.findOne({
+            name: req.params.name
+        })
+        .then(
+            function (disease) {
+                if (!disease) {
+                    res.status(400).send({
+                        success: false,
+                        status: 'Bad Request',
+                        message: 'No disease with this name.'
+                    });
+                }
+                else {
+                    disease.delete(req.decoded._id, 
+                        function () {
+                            res.json({
+                                success: true,
+                                clientMessage: 'Delete disease succeed.',
+                                message: 'Delete disease succeed.'
+                            });
+                        },
+                        function (error) {
+                            res.status(500).send({
+                                success: false,
+                                clientMessage: 'Delete disease failed.',
+                                message: error
+                            });
+                        }
+                    );
+                }
+            },
+            function (error) {
+                res.status(500).send({
+                    success: false,
+                    status: 'Delete disease failed.',
+                    message: error
+                });
+            }
+        );
+    }
+
+    //----------------- ADDITIONAL FUNCTION ----------------- 
+    function valNameateField (res, body) {
+        if (!body.name) {
+            utils.responseMissingField(res, 'name');
+        }
+
+        if (!body.icd10) {
+            utils.responseMissingField(res, 'icd10');
+        }
+
+        if (!body.snowmed) {
+            utils.responseMissingField(res, 'snowmed');
+        }
+
+        if (!body.drg) {
+            utils.responseMissingField(res, 'drg');
+        }
+
+        if (!body.description) {
+            utils.responseMissingField(res, 'description');
+        }
+    }
 };
