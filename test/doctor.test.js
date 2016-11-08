a65a2b2d2e52f0a5514d64e362460418330e4a6b
Nattapong Ousirimaneechai <first852456@gmail.com>
2016-11-08 14:24:03 +0700

    API: set header for test

--- test/doctor.test.js
+++ test/doctor.test.js
@@ -14,6 +14,7 @@ describe("Doctors", function(){
         it("it should GET all doctors", function(done){
             chai.request(server)
             .get('/api/v1/doctors')
+            .set("x-access-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODFkN2RjMmIzMGMxNjFhYjBmMGU5YTIiLCJ1cGRhdGVkQXQiOiIyMDE2LTExLTA1VDA2OjM2OjAwLjYxNFoiLCJjcmVhdGVkQXQiOiIyMDE2LTExLTA1VDA2OjM2OjAwLjYxNFoiLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBkb2NjYXJlLmdvLnRoIiwiX192IjowLCJkZWxldGVkIjpmYWxzZSwibmFtZSI6IuC4nOC4ueC5ieC4lOC4ueC5geC4peC4o-C4sOC4muC4miIsImlhdCI6MTQ3ODU4OTY4MiwiZXhwIjoxNDc4Njc2MDgyfQ.gs2ZX7leY8GcyHDV0mc1aWNOOUns-1NppoDNpEo4DcE")
             .end(
                 function(err, res){
                     res.should.have.status(200);
