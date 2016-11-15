9ffb84944a58eda113d1f16e48ea417ffac69f52
Wipavanee Vatcharadejsakul <ballwipavnee@gmail.com>
2016-11-15 23:34:25 +0700

    nurse test api

--- test/8.nurse.test.js
+++ test/8.nurse.test.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 var mongoose = require('mongoose');
-var Doctor = require('../server/model/Doctor');
+var Nurse = require('../server/model/Nurse');
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
@@ -10,9 +10,8 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 var adminToken = "";
-var staffToken = "";
 var patientToken = "";
-describe("Doctors", function(){
+describe("Nurses", function(){
     before(function(done){
         let p = new Promise(
             function(resolve,reject){
@@ -35,22 +34,6 @@ describe("Doctors", function(){
                 chai.request(server)
                 .post('/auth/login')
                 .send({
-                    username: "staff1",
-                    password: "staff1"
-                })
-                .end(
-                    function(err, res){
-                        staffToken = res.body.data.token;
-                        resolve();
-                    }
-                );
-            }
-        );
-        let r = new Promise(
-            function(resolve,reject){
-                chai.request(server)
-                .post('/auth/login')
-                .send({
                     username: "patient1",
                     password: "patient1"
                 })
@@ -61,8 +44,8 @@ describe("Doctors", function(){
                     }
                 );
             }
-        )
-        Promise.all([p,q,r])
+        );
+        Promise.all([p,q])
         .then(
             function(){
                 done();
@@ -71,18 +54,17 @@ describe("Doctors", function(){
     });
 
 
-    describe("/POST create doctor", function(){
-        it("it should POST create doctor", function(done){
+    describe("/POST create nurse", function(){
+        it("it should POST create nurse", function(done){
             let data = {
-                HN: 'newdoctor',
+                HN: 'newnurse',
                 personalID: '001',
                 preName: 'Mr.',
                 name: 'Benz',
                 surname: 'Thananan',
-                clinic: 'clinic1'
             };
             chai.request(server)
-            .post('/api/v1/doctors')
+            .post('/api/v1/nurses')
             .set("x-access-token",adminToken)
             .send(data)
             .end(
@@ -91,7 +73,7 @@ describe("Doctors", function(){
                     res.should.be.json; 
                     res.body.should.be.a('object');
                     res.body.should.have.property('success',true);
-                    res.body.should.have.property('clientMessage','Create Doctor succeed');
+                    res.body.should.have.property('clientMessage','Create nurse succeed');
                     res.body.should.have.property('data');
                     res.body.data.should.be.a('object');
                     done();
@@ -99,17 +81,17 @@ describe("Doctors", function(){
             );
         });
 
-        it("it should POST create doctor with same HN, so it will be fail.", function(done){
+        it("it should POST create nurse with same HN, so it will be fail.", function(done){
             let data = {
-                HN: 'newdoctor',
-                personalID: '002',
+                HN: 'newnurse',
+                personalID: '001',
                 preName: 'Miss',
                 name: 'Eve',
                 surname: 'Wantanee',
                 clinic: 'clinic1'
             };
             chai.request(server)
-            .post('/api/v1/doctors')
+            .post('/api/v1/nurses')
             .set("x-access-token",adminToken)
             .send(data)
             .end(
@@ -125,31 +107,11 @@ describe("Doctors", function(){
         });
     });
 
-    describe("/GET doctors", function(){
-        it("it should GET all doctors by using admin role", function(done){
+    describe("/GET nurses", function(){
+        it("it should GET all nurses by using admin role", function(done){
             chai.request(server)
-            .get('/api/v1/doctors')
+            .get('/api/v1/nurses')
             .set("x-access-token",adminToken)
-            .end(
-                function(err, res){
-                    res.should.have.status(200);
-                    res.should.be.json; 
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('success',true);
-                    res.body.should.have.property('limit',10);
-                    res.body.should.have.property('data');
-                    res.body.data.should.be.a('array');
-                    res.body.data.should.have.length(10);
-                    // console.log(res.body.data);
-                    done();
-                }
-            )
-        });
-
-        it("it should GET all doctors by using staff role", function(done){
-            chai.request(server)
-            .get('/api/v1/doctors')
-            .set("x-access-token",staffToken)
             .end(
                 function(err, res){
                     res.should.have.status(200);
@@ -168,7 +130,7 @@ describe("Doctors", function(){
 
         it("Using patient role, so it should show error", function(done){
             chai.request(server)
-            .get('/api/v1/doctors')
+            .get('/api/v1/nurses')
             .set("x-access-token",patientToken)
             .end(
                 function(err, res){
@@ -184,11 +146,11 @@ describe("Doctors", function(){
         });
     });
 
-    describe("/GET doctors by HN", function(){
-        it("it should GET doctors by HN using admin role", function(done){
-            let HN = 'newdoctor';
+    describe("/GET nurses by HN", function(){
+        it("it should GET nurses by HN using admin role", function(done){
+            let HN = 'newnurse';
             chai.request(server)
-            .get('/api/v1/doctors/'+HN)
+            .get('/api/v1/nurses/'+HN)
             .set("x-access-token",adminToken)
             .end(
                 function(err, res){
@@ -207,7 +169,7 @@ describe("Doctors", function(){
         it("Using wrong HN to GET/:HN, so it should show error", function(done){
             let HN = '000000';
             chai.request(server)
-            .get('/api/v1/doctors/'+HN)
+            .get('/api/v1/nurses/'+HN)
             .set("x-access-token",adminToken)
             .end(
                 function(err, res){
@@ -215,7 +177,7 @@ describe("Doctors", function(){
                     res.should.be.json; 
                     res.body.should.be.a('object');
                     res.body.should.have.property('message','Bad Request');
-                    res.body.should.have.property('clientMessage', 'No doctor with this HN.');
+                    res.body.should.have.property('clientMessage', 'No nurse with this HN.');
                     // console.log(res.body.data);
                     done();
                 }
@@ -223,11 +185,11 @@ describe("Doctors", function(){
         });
     });
 
-    describe("/PUT update doctor by HN", function(){
-        it("it should PUT create doctor", function(done){
-            let HN = 'newdoctor';
+    describe("/PUT update nurse by HN", function(){
+        it("it should PUT create nurse", function(done){
+            let HN = 'newnurse';
             let data = {
-                HN: 'newdoctor',
+                HN: 'newnurse',
                 personalID: '001',
                 preName: 'Mr.',
                 name: 'BenzUpdate',
@@ -235,7 +197,7 @@ describe("Doctors", function(){
                 clinic: 'clinic1'
             };
             chai.request(server)
-            .put('/api/v1/doctors/'+HN)
+            .put('/api/v1/nurses/'+HN)
             .set("x-access-token",adminToken)
             .send(data)
             .end(
@@ -244,7 +206,7 @@ describe("Doctors", function(){
                     res.should.be.json; 
                     res.body.should.be.a('object');
                     res.body.should.have.property('success',true);
-                    res.body.should.have.property('clientMessage','Update Doctor succeed');
+                    res.body.should.have.property('clientMessage','Update nurse succeed');
                     res.body.should.have.property('data');
                     res.body.data.should.be.a('object');
                     done();
@@ -253,11 +215,11 @@ describe("Doctors", function(){
         });
     });
 
-    describe("/DELETE delete doctor by HN", function(){
-        it("it should Delete create doctor", function(done){
-            let HN = 'newdoctor';
+    describe("/DELETE delete nurse by HN", function(){
+        it("it should Delete create nurse", function(done){
+            let HN = 'newnurse';
             chai.request(server)
-            .delete('/api/v1/doctors/'+HN)
+            .delete('/api/v1/nurses/'+HN)
             .set("x-access-token",adminToken)
             .end(
                 function(err, res){
@@ -265,8 +227,8 @@ describe("Doctors", function(){
                     // res.should.be.json; 
                     // res.body.should.be.a('object');
                     res.body.should.have.property('success',true);
-                    res.body.should.have.property('clientMessage','Delete Doctor succeed.');
-                    res.body.should.have.property('message','Delete Doctor succeed.');
+                    res.body.should.have.property('clientMessage','Delete nurse succeed.');
+                    res.body.should.have.property('message','Delete nurse succeed.');
                     done();
                 }
             );
